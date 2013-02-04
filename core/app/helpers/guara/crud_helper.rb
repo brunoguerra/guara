23f37a16c1e98a0d65fec5ad21a875ef2105bd4c
Felipe <felipe@felipe-Aspire-5750.(none)>
2013-02-04 14:05:53 -0300

    Modificação layout cliente/contatos

--- core/app/helpers/guara/crud_helper.rb
+++ core/app/helpers/guara/crud_helper.rb
@@ -7,6 +7,12 @@ module Guara
   		
   	end
 
+  	def index_btn_edit(path, title="")
+
+  		link_to raw('<i  class="icon-envelope"></i>'), path, :class => "btn ", :title => title
+  		
+  	end
+
 
     def show_label_tag(label)
       label_tag label, label+":", :class => "strong"
