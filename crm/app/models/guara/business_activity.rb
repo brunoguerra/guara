53f3a2a91c20954ba88d7b1983d9c3bd7d714ce6
brunoguerra <ti@woese.com>
2013-02-26 15:20:34 -0300

    initialize widget schedule professionals

--- crm/app/models/guara/business_activity.rb
+++ crm/app/models/guara/business_activity.rb
@@ -1,7 +1,7 @@
 module Guara
   class BusinessActivity < ActiveRecord::Base
-    attr_accessible :enabled, :name, :business_segment, :notes
-  
+    attr_accessible :enabled, :name, :business_segment, :business_segment_id, :notes
+    
     belongs_to :business_segment
   end
 end
