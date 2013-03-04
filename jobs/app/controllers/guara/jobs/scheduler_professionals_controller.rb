125f45d1685b1045ccb26ec67a1b6350789367ce
brunoguerra <ti@woese.com>
2013-03-04 13:14:03 -0300

    engine load active_process lib

--- jobs/app/controllers/guara/jobs/scheduler_professionals_controller.rb
+++ jobs/app/controllers/guara/jobs/scheduler_professionals_controller.rb
@@ -11,9 +11,9 @@ module Guara
       end
       
       def load_selecteds_professionals()
-        @vacancy = @vacancy || Vacancy.find_by_process_instance(params[:process_instance_id])
-        @unscheduleds = @vacancy.unscheduled_professionals()
-        @scheduleds = @vacancy.scheduled_professionals()
+        @vacancy      = @vacancy || Vacancy.find_by_process_instance(params[:process_instance_id])
+        @unscheduleds = VacancySchedulingProfessional.unscheduled_professionals(@vacancy)
+        @scheduleds   = VacancySchedulingProfessional.scheduled_professionals(@vacancy)
       end
       
       def index
--- jobs/app/models/guara/jobs/vacancy_scheduling_professional.rb
+++ jobs/app/models/guara/jobs/vacancy_scheduling_professional.rb
@@ -3,14 +3,22 @@ module Guara
     class VacancySchedulingProfessional < ActiveRecord::Base
       attr_accessible :date_time, :local, :consultant_id, :vacancy_id, :professional_id
 
-
-
-
       belongs_to :process_instance
       belongs_to :consultant
       belongs_to :vacancy
       has_many :professionals
+     
+      def self.unscheduled_professionals(vacancy)
+        vacancy.selection_professionals_selecteds().reject! do |p|
+          VacancySchedulingProfessional.where(vacancy_id: vacancy.id, professional_id: p.id).count()==0
+        end
+      end
       
+      def self.scheduled_professionals(vacancy)
+        vacancy.selection_professionals_selecteds().reject! do |p|
+          VacancySchedulingProfessional.where(vacancy_id: vacancy.id, professional_id: p.id).count()>0
+        end
+      end
      
     end
   end
