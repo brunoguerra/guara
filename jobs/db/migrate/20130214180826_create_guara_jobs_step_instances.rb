0c003587b74199e7e77c31bdfc36f135fc795f6d
Igor Moura <igorsilvadm@hotmail.com>
2013-05-04 15:07:35 -0300

    removing extra migrations

--- jobs/db/migrate/20130214180826_create_guara_jobs_step_instances.rb
+++
@@ -1,8 +0,0 @@
-class CreateGuaraJobsStepInstances < ActiveRecord::Migration
-  def change
-    create_table :guara_jobs_step_instances do |t|
-      t.integer :process_instance_id
-      t.timestamps
-    end
-  end
-end
--- jobs/db/migrate/20130328135754_add_column_interested_to_vacancy_scheduling_professional.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnInterestedToVacancySchedulingProfessional < ActiveRecord::Migration
-  def change
-    add_column :guara_jobs_vacancy_scheduling_professionals, :interested, :boolean
-
-  end
-end
