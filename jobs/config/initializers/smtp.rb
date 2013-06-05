38fb7229e901f5f9768ea10a220d74325cf689ce
maciel-woese <maciel@woese.com>
2013-06-05 14:20:24 -0300

    Initialize Smtp Send Email

---
+++ jobs/config/initializers/smtp.rb
@@ -0,0 +1,9 @@
+  GuaraCmgb::Application.config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'localhost:3000',
+    :user_name            => "maciel@woese.com",
+    :password             => "*********",
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  
+  }
