a62f2289a1e2dcb6f53f17a4be4aaeb89af22d9b
maciel-woese <maciel@woese.com>
2013-06-17 13:48:33 -0300

    Atualização Jobs

--- jobs/spec/factories.rb
+++ jobs/spec/factories.rb
@@ -1,12 +1,21 @@
 puts "loading jobs/factories..."
 
 #core factories
-load Rails.root.join('../../../crm/spec/factories.rb')
+load Rails.root.join('../../../crm/spec/factories.rb') if ENV['factories_dependencies']!='false'
 
 FactoryGirl.define do
 
   factory :custom_process, :class => Guara::Jobs::CustomProcess do
     sequence(:name) { |n| "#{Faker::Name.name}#{n}" }
+  end
+
+  factory :simple_customer, :class => Guara::Person do
+  	name { Faker::Name.name }
+  	doc { '' }
+  end
+
+  factory :professional, :class => Guara::Jobs::Professional do
+	association :person, factory: :simple_customer
   end
   
 end
