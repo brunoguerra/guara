2020115bbd6af4216775289e1dbb332c63dc102b
brunoguerra <ti@woese.com>
2013-07-22 10:54:18 -0300

    messages JS

--- core/app/assets/javascripts/guara/core.js
+++ core/app/assets/javascripts/guara/core.js
@@ -83,8 +83,19 @@ $(document).ready(function(){
 
 });
 
+function displayMessage(msg, id, time, type) {
+  if (time==null) time = 8000; 
+  if (type==null) type = "error"; 
+  if ($('#'+id).size()==0) {
+      var div = $('<div class="alert alert-'+type+' hide" id="'+id+'">'+msg+'</div>');
+      $('section.container').prepend(div);
+      div.fadeIn().delay(time).fadeOut();
+      setTimeout(function(){ div.remove(); }, time+500);
+    }
+}
+
 function showMessage(message_content) {
-	$(".alert-ajax").show().html(message_content);
+	displayMessage(message_content, "showMessage", "warning");
 	$(document).scrollTop();
 }
 
--- crm/app/assets/javascripts/guara/crm.js
+++ crm/app/assets/javascripts/guara/crm.js
@@ -6,83 +6,95 @@ jQuery(function() {
   });
 
   $('form, div.list_nested_form').on('click', '.add_fields', function(event) {
-    var regexp, time;
-    time = new Date().getTime();
-    regexp = new RegExp($(this).data('id'), 'g');
+    var time = new Date().getTime(),
+        regexp = new RegExp($(this).data('id'), 'g');    
+    
     $(this).before($(this).data('fields').replace(regexp, time));
     return event.preventDefault();
   });
 
   $('input.readonly').on('keydown', function(e){
-    return false;
+    var k = (e.which) ? e.which : e.keyCode;
+    if ([9,13].indexOf(k) == -1) return false;
   });
 
-  $(document).on('ready', function(event) {
-    return $("select.multiselect").each(function(i) {
-      var inp, select, url_json, placeholder;
-      if ($(this).attr("data-placeholder") != null) {
-        placeholder = $(this).attr("data-placeholder");
-      }
+  
+  $("select.multiselect").each(function(i) {
+    var inp, 
+        select, 
+        url_json,
+        placeholder;
 
-      if ($(this).attr("data-json-url") != null) {
-        url_json = $(this).attr("data-json-url");
-        inp = $("<input>").attr("id", $(this).name).attr('data-ajax', 1).attr("class", $(this).attr("class")).val($(this).val());
-        select = $(this);
-        $($(this).parent()).append(inp);
-        $(inp).select2({
-          maximumInputLength: 10,
-          placeholder: placeholder,
-          ajax: {
-            data: function(term, page) {
-              return {
-                search: term,
-                page_limit: 10
-              };
-            },
-            dataType: 'json',
-            results: function(data, term) {
-              return {
-                results: data
-              };
-            },
-            url: url_json
+    if ($(this).attr("data-placeholder") != null) {
+      placeholder = $(this).attr("data-placeholder");
+    }
+
+    if ($(this).attr("data-json-url") != null) {
+      url_json = $(this).attr("data-json-url");
+      inp = $("<input>").attr("id", $(this).name+'-input-ajax').attr('data-ajax', 1).attr("class", $(this).attr("class")).val($(this).val());
+      select = $(this);
+      $($(this).parent()).append(inp);
+      $(inp).select2({
+        maximumInputLength: 20,
+        placeholder: placeholder,
+        ajax: {
+          data: function(term, page) {
+            return {
+              search: term,
+              page_limit: 10
+            };
           },
-          initSelection: function(element, callback) {
-            var data, id;
-            id = $(element).val();
-            if (id !== "") {
-              data = {
-                id: $(select.children()[0]).attr('value'),
-                name: $(select.children()[0]).text()
-              };
-              return callback(data);
-            }
+          dataType: 'json',
+          results: function(data, term) {
+            return {
+              results: data
+            };
           },
-          formatResult: function(data) {
-            return data.name;
-          },
-          formatSelection: function(data) {
-            var opt;
-            opt = $(select.children()[0]);
-            if ((!opt) || (opt.attr('id') !== data.id)) {
-              opt.remove();
-              select.append($('<option></option>').attr('value', data.id).text(data.name));
-            }
-            select.val(data.id);
-            return data.name;
-          },
-          dropdownCssClass: "bigdrop",
-          escapeMarkup: function(m) {
-            return m;
+          url: url_json
+        },
+        initSelection: function(element, callback) {
+          var data, id;
+          id = $(element).val();
+          if (id !== "") {
+            data = {
+              id: $(select.children()[0]).attr('value'),
+              name: $(select.children()[0]).text()
+            };
+            return callback(data);
           }
-        });
-        return $(this).hide();
-      } else {
-        return $(this).select2({
-          maximumInputLength: 10,
-          placeholder: placeholder,
-        });
-      }
-    });
+        },
+        formatResult: function(data) {
+          return data.name;
+        },
+        formatSelection: function(data) {
+          var opt,
+              event;
+
+          opt = $(select.children()[0]);
+
+          if ((!opt) || (opt.attr('id') !== data.id)) {
+            opt.remove();
+            select.append($('<option></option>').attr('value', data.id).text(data.name));
+          }
+          select.val(data.id);
+          
+          /*event*/
+          event = jQuery.Event("change", { val: data.id });
+          select.trigger(event);
+
+          return data.name;
+        },
+        dropdownCssClass: "bigdrop",
+        escapeMarkup: function(m) {
+          return m;
+        }
+      });
+      return $(this).hide();
+    } else {
+      return $(this).select2({
+        maximumInputLength: 20,
+        placeholder: placeholder,
+      });
+    }
   });
 });
